3cb240098e894c5ac24dd3a8f2ecd3e8f268214f
monica-shi <monica070706@gmail.com>
2024-01-20 17:08:16 -0500

    remove hard coded paths

--- karting/middleware.py
+++ karting/middleware.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, resolve
 
 
 class AuthRequiredMiddleware(object):
@@ -13,8 +13,7 @@ class AuthRequiredMiddleware(object):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
-        print(request.path)
-        if not request.user.is_authenticated and request.path != '/accounts/login/':
+        if not request.user.is_authenticated and resolve(request.path).view_name != 'login':
             return HttpResponseRedirect(reverse_lazy('login'))  # or http response
 
         response = self.get_response(request)
