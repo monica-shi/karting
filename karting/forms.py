1e2879d123e49b122092643b0afd319b81d3e277
monica-shi <monica070706@gmail.com>
2024-04-12 23:01:05 -0400

    Add new users to 'default' group

--- karting/forms.py
+++ karting/forms.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 
 class SignupForm(UserCreationForm):
+
+    def save(self, commit=True):
+        user = super().save(commit)
+        if commit:
+            default_group = Group.objects.get(name='default')
+            default_group.user_set.add(user)
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
